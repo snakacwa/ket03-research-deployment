ff26ec47bbaf54d416d491d4e8a50ce5e440a046
Sandra Nakacwa <93861613+snakacwa@users.noreply.github.com>
2025-06-23 18:42:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ from sklearn.decomposition import PCA
 app = Flask(__name__)
 
 # Load already-trained models
-scaler = joblib.load("scaler.pkl")
-pca = joblib.load("pca_model.pkl")
+scaler = joblib.load("scaler_ket03.pkl")
+pca = joblib.load("pca_model_ket03.pkl")
 model = joblib.load("rf_classifier_3min_final.pkl")
 
 # Expected features
